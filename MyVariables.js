09cbfc93242e9a67270b89925eb4e12d8da7c051
Rob In der Maur <rmaur@adobe.com>
2022-12-01 18:10:39 +0100

    Adobe Analytics measurement files and basic implementation

---
+++ MyVariables.js
@@ -0,0 +1,17 @@
+// instantiating the analytics tracking object
+var s_account = "adobecx1ridm";
+var s = s_gi(s_account);
+
+// required variable; sets the cookie domain
+// stick with it to just not reset your visitors
+s.trackingServer = "ridm.data.adobedc.net";
+s.pageName = "RIdM Home Page";
+s.eVar1 = "Exanple value";
+s.prop1 = "Prop";
+
+// send the data to the report suite
+s.t();
+
+
+
+
