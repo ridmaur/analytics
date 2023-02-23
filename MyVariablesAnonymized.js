91916478c051210f74d29ab83f79183227067514
Rob In der Maur <rmaur@adobe.com>
2023-02-23 10:01:40 +0100

    Changes

--- MyVariablesAnonymized.js
+++ MyVariablesAnonymized.js
@@ -5,9 +5,10 @@ var s = s_gi(s_account);
 // required variable; sets the cookie domain
 // stick with it to just not reset your visitors
 s.trackingServer = "ridm.data.adobedc.net";
-s.pageName = "Implement Adobe Analytics Using Javascript - AppMeasurement";
-s.eVar1 = "Exanple value";
-s.prop1 = "Prop";
+s.pageName = "Implement Adobe Analytics Using Javascript - AppMeasurement (Anonymized)";
+s.eVar1 = "Exanple value (Anonymized)";
+s.prop1 = "Prop (Anonymized)";
+s.visitorID = "0"
 
 // send the data to the report suite
 s.t();
